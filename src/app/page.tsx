2da7fff643a37feb3675b7a4fb6fb30c22a2a3d9
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-03-07 09:05:59 +0000

    feat: setup core UI layout, global styles, and navbar structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,10 @@
+import Navbar from "@/components/layout/Navbar";
 
+export default function Page() {
+	return (
+		<div>
+			<Navbar />
+			<h1>Welcome to MY App</h1>
+		</div>
+	);
+}
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,9 @@
+import Logo from "@/components/shared/logo";
+
+export default function Navbar() {
+	return (
+		<div>
+			<Logo />		
+		</div>
+	);
+}

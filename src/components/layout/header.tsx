93a8cf94b822bde595701d63e7f754e9fd7105ce
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-04-05 09:53:15 +0000

    chore(header): rename navbar.tsx to specific desktop-nav.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 import { Logo } from "../shared/logo";
 import { CartDrawer } from "./cart";
 import MobileNav from "./mobile-nav";
-import { Navbar } from "./navbar";
+import { Navbar } from "./desktop-nav";
 import { SearchBar } from "./search-bar";
 
 export function Header() {

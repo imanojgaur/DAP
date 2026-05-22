355e92919ecc5e6ad73a15769e39db393ff6875c
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-05-22 08:52:34 +0000

    refactor(config): move constants to confg

--- src/config/nav-config.ts
+++ src/config/nav-config.ts
@@ -1,4 +1,4 @@
-import { ROUTES } from "@/lib/constants/routes";
+import { ROUTES } from "@/config/constants/routes";
 import type { LogoConfig, NavItem, SiteName } from "@/types/nav";
 
 const siteName: SiteName = "DAP";
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
-export * from "./constants/routes";
+export * from "../config/constants/routes";
 export * from "./price";
 export * from "./string-formater";

facd9b880d4cddc6e08761ae3cadced94bd161d1
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-06-16 14:29:18 +0000

    perf(actions): convert to named exports

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,10 @@
-export * from "./predict";
-export * from "./recommend-plant";
-export * from "./search";
+export { getCheckoutAddresses, saveAddressAction } from "./address";
+export { syncCartAction } from "./cart";
+export { getFeaturedProducts, getHomeCategories} from "./home";
+export { getLogsAction, writeLogAction } from "./logger";
+export { verifyRazorpayPaymentAction } from "./payment-verify";
+export { placeOrderAction } from "./place-order";
+export { getCropPrediction } from "./predict";
+export { createRazorpayOrder } from "./razorpay-orders";
+export { getUnifiedSearchData } from "./search";
+export { getCustomerOrders, getAllAdminOrders, updateOrderStatus} from "./view-orders";

66de2fa911ad6ca0c57532c881ed8eff041b2062
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-07-15 13:15:54 +0000

    feat(type): export CategoryCardProps interface

--- src/components/home/category-card.tsx
+++ src/components/home/category-card.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-interface CategoryCardProps {
+export interface CategoryCardProps {
     name: string;
     slug: string;
     productCount: number;

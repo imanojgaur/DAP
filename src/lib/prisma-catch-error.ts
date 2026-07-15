a5723ab00c56910f05fcecfd84015c3c090be085
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-07-15 14:31:36 +0000

    chor(db): add db error utils boilerplate

---
+++ src/lib/prisma-catch-error.ts
@@ -0,0 +1,13 @@
+interface functionPassed {
+    function: () => <T>
+
+}
+export function withPrismaQuery<T>(functionCall: ){
+    try{
+        const data = functionCall();
+        const error = null;
+
+        return {data, error}
+        
+    }
+}

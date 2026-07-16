6a7cace2493d952d60ffa4cf8ebe9761cdf445dd
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-07-16 16:16:18 +0000

    fix(42P01): force postgres to read _CategoryToProduct as it is

--- src/data-sql/home.ts
+++ src/data-sql/home.ts
@@ -17,7 +17,7 @@ export async function getHomeCategories(categories: string[]){
         c.name, 
         COUNT(jcp.B) OVER(PARTITION BY c.id) AS product_count
         FROM categories c
-        INNER JOIN _CategoryToProduct jcp ON c.id = jcp.A 
+        INNER JOIN "_CategoryToProduct" jcp ON c.id = jcp.A 
         WHERE c.name IN (${Prisma.join(categories)}) ; -- create secure parameter: ($1, $2, $3)
         `
         return data;

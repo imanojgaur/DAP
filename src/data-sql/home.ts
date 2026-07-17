fbd3d7e4d1bba0eb133f3f5f7b4e5325ef60401a
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-07-17 02:20:02 +0000

    fix(db): add quotes to capatalized table and column name

--- src/data-sql/home.ts
+++ src/data-sql/home.ts
@@ -15,9 +15,9 @@ export async function getHomeCategories(categories: string[]){
         SELECT 
         c.slug, 
         c.name, 
-        COUNT(jcp.B) OVER(PARTITION BY c.id) AS product_count
+        COUNT(jcp."A") OVER(PARTITION BY c.slug) AS product_count
         FROM categories c
-        INNER JOIN "_CategoryToProduct" jcp ON c.id = jcp.A 
+        INNER JOIN "_CategoryToProduct" jcp ON c.id = jcp."A"
         WHERE c.name IN (${Prisma.join(categories)}) ; -- create secure parameter: ($1, $2, $3)
         `
         return data;

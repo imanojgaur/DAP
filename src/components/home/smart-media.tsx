fe4134e6ca1094afe8a4811c3ba0ee81a44adeee
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-07-31 16:47:01 +0000

    ref(type): export props type interface

--- src/components/home/smart-media.tsx
+++ src/components/home/smart-media.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { CldImage } from "next-cloudinary";
 
-interface SmartMediaProps {
+export interface SmartMediaProps {
     sourceType: 'cloudinary' | 'nextServer'
     src: string, 
     width?: number, 

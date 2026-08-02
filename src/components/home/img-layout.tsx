376035d327a0bf2d1548c848c5ac81f1d5896275
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-08-02 08:06:22 +0000

    feat(ui): setup grid image layout for different image count

--- src/components/home/img-layout.tsx
+++ src/components/home/img-layout.tsx
@@ -6,29 +6,62 @@ import { SmartMedia, type SmartMediaProps } from "./smart-media";
 type  ImageLayoutProps = DistributiveOmit<SmartMediaProps, 'fill' | 'className' | 'sizes'> & {isPrimary?: boolean}
 
 export function DynamicImageGrid ({images}:{images: ImageLayoutProps[]}) {
+    const isPrimary = images.find((image) => image.isPrimary) || images[0]
+    const secondaryImages = images.filter((image) => !image.isPrimary).slice(0,4)
+
+    const noOfImages = secondaryImages.length
+    let secImgsLayout = ''
+
+    switch(noOfImages){
+        case 2:
+            secImgsLayout = 'flex flex-row overflow-x-auto snap-x snap-mendatory no-scrollbar'
+            break
+        case 3:
+            secImgsLayout = 'grid grid-col-2 grid-row-2 gap-0 snap-x snap-mendatory'
+            break
+        default:
+            secImgsLayout = 'grid grid-col-2 grid-row-2 gap-0 snap-x snap-mendatory'
+    }
 
     return (
-        <div>
-            {images.map((image) => (
+        <div 
+        style={`--grid-layout:${secImgsLayout}` as React.CSSProperties} 
+        className="absolute z-0 w-full h-full overflow-hidden"
+        >
+            <div className="relative w-full h-full z-10 group-hover:z-0 overflow-hidden">
                 <SmartMedia 
-                key={image.sourceType === 'cloudinary'? image.publicId: image.imageSrc}
-                {...image}
+                {...isPrimary}
                 fill
                 className="object-cover transform-gpu transition-transform duration-[2000ms] ease-out group-hover:scale-110"
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 />
-            ))}
+            </div>
+
+            {secondaryImages.length > 0 && <div className={`absolute w-full h-full inset-0 z-0 group-hover:z-10 var(--grid-layout) overflow-hidden`}>
+                {secondaryImages.map((image) => (
+                    <div key={image.sourceType === 'cloudinary'? image.publicId: image.imageSrc}
+                    className="relative"
+                    >
+                        <SmartMedia 
+                        {...image}
+                        fill
+                        className="object-cover transform-gpu transition-transform duration-[2000ms] ease-out group-hover:scale-110"
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        />
+                    </div>
+                ))}
+            </div>}
         </div>
     )
 }
 
 export function HorizontalImgScroller ({images}:{images: ImageLayoutProps[]}) {
     const primaryImage = images.find((image) => image.isPrimary) || images[0]
-    const secondaryImages = images.filter((image) => !image.isPrimary).slice(0,4)
+    const secondaryImages = images.filter((image) => !image.isPrimary)
     return (
-        <div className="absolute inset-0 z-0 flex flex-row overflow-x-auoto snap-x snap-mandatory no-scrollbar">
+        <div className="absolute inset-0 z-0 flex flex-row overflow-x-auto snap-x snap-mandatory no-scrollbar">
             
-            {primaryImage && <div className="relative w-full h-full snap-o snap-center overflow-hidden">
+            {primaryImage && <div className="relative w-full h-full snap-0 snap-center overflow-hidden">
                 <SmartMedia
                 {...primaryImage}
                 fill 
@@ -38,7 +71,7 @@ export function HorizontalImgScroller ({images}:{images: ImageLayoutProps[]}) {
             </div>
             }
 
-            {secondaryImages?.map((image) => (
+            {secondaryImages.length > 0 && secondaryImages.map((image) => (
                 // Give Image a physical wrapper to not let stack
                 <div key={image.sourceType === 'cloudinary'? image.publicId: image.imageSrc}
                 className="relative w-full h-full snap-0 snap-center overflow-hidden"

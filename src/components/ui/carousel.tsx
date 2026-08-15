ef502ee119daa3d958193ca1b140b7d641474126
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-08-15 12:16:04 +0000

    fix(ui): handle card scroll to gpu using tailwind will-change-transform

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -142,7 +142,7 @@ function CarouselContent({ className, ...props }: React.ComponentProps<"div">) {
 		>
 			<div
 				className={cn(
-					"flex",
+					"flex will-change-transform",
 					orientation === "horizontal" ? "-ml-4" : "-mt-4 flex-col",
 					className,
 				)}
@@ -152,7 +152,7 @@ function CarouselContent({ className, ...props }: React.ComponentProps<"div">) {
 	);
 }
 
-function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
+function 	CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
 	const { orientation } = useCarousel();
 
 	return (
@@ -161,7 +161,7 @@ function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
 			aria-roledescription="slide"
 			data-slot="carousel-item"
 			className={cn(
-				"min-w-0 shrink-0 grow-0 basis-full",
+				"min-w-0 shrink-0 grow-0 basis-full will-change-transform",
 				orientation === "horizontal" ? "pl-4" : "pt-4",
 				className,
 			)}

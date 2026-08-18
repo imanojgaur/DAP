3ddf5d64f9b13171bb2caf26f868d3eb0faafc20
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-08-18 09:24:56 +0000

    ref(type): declare children readonly

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -3,9 +3,7 @@ import { Footer } from "@/components/layout/footer";
 
 export default function ShopLayout({
 	children,
-}: {
-	children: React.ReactNode;
-}) {
+}: Readonly<{children: React.ReactNode}>) {
 	return (
 		<div className="flex flex-col min-h-screen">
 			{/* <Header /> */}

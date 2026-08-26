ba27e8503869da1c01bca51773ecffa2ced71798
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-08-26 06:52:31 +0000

    feat(ui): import header

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,12 +1,12 @@
 import { Footer } from "@/components/layout/footer";
-// import { Header } from "@/components/layout/header";
+import { Header } from "@/components/layout/header";
 
 export default function ShopLayout({
 	children,
 }: Readonly<{children: React.ReactNode}>) {
 	return (
 		<div className="flex flex-col min-h-screen">
-			{/* <Header /> */}
+			<Header />
 			<main className="flex-grow">{children}</main>
 			<Footer />
 		</div>

1dd443f8f4904447d6ecdf2dec53397a2837af56
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-08-28 14:14:17 +0000

    fix(ui): animate drawer emerge from header
    tweak z-index and data-state

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -106,14 +106,16 @@ function NavigationMenuViewport({
 	return (
 		<div
 			className={cn(
-				"absolute top-full left-1/2 -translate-x-1/2 isolate z-50 flex justify-center w-screen",
+				"absolute top-full left-1/2 -translate-x-1/2 isolate -z-20 flex justify-center w-screen",
 			)}
 		>
 			<NavigationMenuPrimitive.Viewport
 				data-slot="navigation-menu-viewport"
 				className={cn(
-					// Make sure this string ends cleanly at zoom-in-90
-					"origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden border-b border-gray-100 bg-white text-popover-foreground shadow-xl data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:zoom-in-90",
+					"origin-top relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden border-b border-gray-100 bg-white text-popover-foreground shadow-xl",
+					"data-[state=open]:animate-in data-[state=open]:fade-in data-[state=open]:slide-in-from-top-30",
+					" data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=closed]:slide-out-to-top-30",
+					"transition-all ease-in-out duration-400",
 					className,
 				)}
 				{...props}

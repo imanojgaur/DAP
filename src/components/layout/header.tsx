9813f45382e7dd4ce764ed4bc060860524ea3f8c
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-08-29 07:15:07 +0000

    feat(ui): blur back smoothly at drawer close

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,15 +7,15 @@ export function Header({DesktopNav}:{DesktopNav:React.ReactNode}){
         <header className="group fixed left-0 top-0 z-50 w-full">
 
             {/* Shield: Dropdown text move blow this getting invisible into*/}
-            <div className="absolute inset-0 w-full h-full bg-white/80 backdrop-blur-md transition-colors group-has-[[data-state=open]]:bg-white group-has-[[data-state=open]]:duration-0 group-has-[[data-state=open]]:delay-0 duration-0 delay-[400ms]"/>
+            <div className="absolute inset-0 w-full h-full bg-white/80 backdrop-blur-md transition-all group-has-[[data-state=open]]:bg-white group-has-[[data-state=open]]:duration-300 group-has-[[data-state=open]]:delay-0 duration-[1500ms] delay-[200ms] ease-in"/>
             
             {/* 1. THE PAGE BLUR OVERLAY: Fades in ONLY when the group has an open menu*/}
-            <div className="absolute top-full left-0 w-screen h-[100vh] bg-black/50 backdrop-blur-md -z-30 opacity-0 pointer-events-none transition-opacity duration-500 group-has-[[data-state=open]]:opacity-100" />
+            <div className="absolute top-full left-0 w-screen h-[100vh] bg-black/50 -z-30 opacity-0 pointer-events-none transition-opacity duration-500 group-has-[[data-state=open]]:opacity-100" />
 
             <div className="relative w-full max-w-[1920px] mx-auto">
 
                 {/* Eybrow Nav (Help & user Account) */}
-                <div className="hidden md:flex gap-2 justify-end items-center h-8 bg-[#f5f5f5] px-[2vw] font-[500] text-sm">
+                <div className="hidden md:flex gap-2 justify-end items-center h-8 px-[2vw] font-[500] text-sm bg-transparent border-b border-gray/5">
 
                     <a href="/help">Help</a>
 

f2bec30c2dd7a90fe7b5d825badc93b4df1af66e
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-08-31 16:01:33 +0000

    refactor(type): add global store types

--- src/store/cart-store.ts
+++ src/store/cart-store.ts
@@ -2,7 +2,13 @@
 
 import { create } from 'zustand'
 
-export const useCart = create((set) => ({
+interface Store {
+    productCount: number, 
+    addProduct: () => void, 
+    removeProduct: () => void, 
+ }
+
+export const useCart = create<Store>((set) => ({
     productCount: 0, 
     addProduct: () => set((state)=> ({productCount: state.productCount + 1})), 
     removeProduct: () => set((state)=>({productCount: state.productCount - 1}))

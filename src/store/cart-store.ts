7c977aa803d8ae75bd5b3ee6aa2a0a45f7da5d6f
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-09-10 15:49:45 +0000

    feat(cart): add remove, incrementQuantity, decrementQuantity functions for cart item

--- src/store/cart-store.ts
+++ src/store/cart-store.ts
@@ -19,8 +19,10 @@ interface Store {
     productCount: number,
     isDrawerOpen: boolean, 
     setDrawerOpen: (isOpen: boolean) => void, 
-    addProduct: (productInfo: ProductInfo) => void, 
-    removeProduct: (id: string) => void, 
+    addNewItem: (productInfo: ProductInfo) => void, 
+    removeItem: (id: string) => void, 
+    increaseQuantity: (id: string) => void, 
+    decreaseQuantity: (id: string) => void, 
 }
 
 export const useCart = create<Store>((set) => ({
@@ -30,23 +32,39 @@ export const useCart = create<Store>((set) => ({
     isDrawerOpen: false, 
     setDrawerOpen: (isOpen) => set(() => ({isDrawerOpen: isOpen})), 
 
-    addProduct: (productInfo) => set((state)=> {
-        const oldItems = state.items.filter((oldItem) => oldItem.id !== productInfo.id )
-        const existingItem = state.items.find((oldItem) => oldItem.id === productInfo.id)
-
+    addNewItem: (productInfo) => set((state)=> {
         return ({ 
-        items: [
-            ...oldItems, 
-            existingItem // if item already exists
-            ? {...existingItem, itemCount: existingItem.itemCount + 1} 
-            : {...productInfo, itemCount: 1}
-        ], 
+        items: [...state.items, {...productInfo, itemCount: 1}], 
+        productCount: state.productCount + 1, 
         isDrawerOpen: true, 
-        productCount: state.items.length + 1
     })}), 
     
-    removeProduct: (id) => set((state)=>({
-        items: state.items.filter((item) => item.id === id), 
-        productCount: state.productCount - 1
-    }))
+    removeItem: (id) => set((state) => (
+        {
+            items: state.items.filter((item) => item.id === id), 
+            productCount: state.productCount - 1
+        }
+    )), 
+
+    increaseQuantity: (id) => set((state) => (
+        { 
+            items: state.items.map((item) => (
+                item.id === id
+                    ? {...item, itemCount: item.itemCount + 1}
+                    : item
+            )), 
+            productCount: state.productCount + 1
+        }
+    )),
+
+    decreaseQuantity: (id) => set((state) => (
+        {
+            items: state.items.map((item) => (
+                item.id === id
+                ? {...item, itemCount: item.itemCount - 1}
+                : item
+            )),
+            productCount: state.productCount - 1
+        }
+    ))
 }))

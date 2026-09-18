5500d892d3c8bdd1ff398a8547f5066a0a85a79b
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-09-18 16:53:07 +0000

    chore(db): remove default seed file

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,13 @@
+import { config } from "dotenv"
 import { defineConfig } from "prisma/config";
+
+config({path:".env.local"})
 
 export default defineConfig({
 	schema: "prisma/schema.prisma",
 	migrations: {
 		path: "prisma/migrations",
-		seed: `tsx prisma/seed.ts`,
+		// seed: `tsx prisma/seed.ts`,
 	},
 	datasource: {
 		url: process.env.DIRECT_URL,
